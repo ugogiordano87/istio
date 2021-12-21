979fe788eec6e5f4842c5469e387a3efafb29b7f
Ugo Giordano <ugiordan@redhat.com>
2021-12-21 19:28:24 +0100

    added support for Oracle DB

--- samples/bookinfo/src/ratings/ratings.js
+++ samples/bookinfo/src/ratings/ratings.js
@@ -46,166 +46,223 @@ if (process.env.SERVICE_VERSION === 'v-unhealthy') {
  * We default to using mongodb, if DB_TYPE is not set to mysql.
  */
 if (process.env.SERVICE_VERSION === 'v2') {
-  if (process.env.DB_TYPE === 'mysql') {
-    var mysql = require('mysql')
-    var hostName = process.env.MYSQL_DB_HOST
-    var portNumber = process.env.MYSQL_DB_PORT
-    var username = process.env.MYSQL_DB_USER
-    var password = process.env.MYSQL_DB_PASSWORD
-  } else {
-    var MongoClient = require('mongodb').MongoClient
-    var url = process.env.MONGO_DB_URL
-  }
+    if (process.env.DB_TYPE === 'mysql') {
+        var mysql = require('mysql')
+        var hostName = process.env.MYSQL_DB_HOST
+        var portNumber = process.env.MYSQL_DB_PORT
+        var username = process.env.MYSQL_DB_USER
+        var password = process.env.MYSQL_DB_PASSWORD
+    } else if (process.env.DB_TYPE === 'oracle') {
+        var oracledb = require('oracledb');
+        var dbConfig = {
+            user: process.env.ORACLE_DB_USER,
+            password: process.env.ORACLE_DB_PASSWORD,
+            connectString: process.env.ORACLE_DB_CONNECT_STRING
+        };
+    } else {
+        var MongoClient = require('mongodb').MongoClient
+        var url = process.env.MONGO_DB_URL
+    }
 }
 
 dispatcher.onPost(/^\/ratings\/[0-9]*/, function (req, res) {
-  var productIdStr = req.url.split('/').pop()
-  var productId = parseInt(productIdStr)
-  var ratings = {}
+    var productIdStr = req.url.split('/').pop()
+    var productId = parseInt(productIdStr)
+    var ratings = {}
 
-  if (Number.isNaN(productId)) {
-    res.writeHead(400, {'Content-type': 'application/json'})
-    res.end(JSON.stringify({error: 'please provide numeric product ID'}))
-    return
-  }
+    if (Number.isNaN(productId)) {
+        res.writeHead(400, {'Content-type': 'application/json'})
+        res.end(JSON.stringify({error: 'please provide numeric product ID'}))
+        return
+    }
 
-  try {
-    ratings = JSON.parse(req.body)
-  } catch (error) {
-    res.writeHead(400, {'Content-type': 'application/json'})
-    res.end(JSON.stringify({error: 'please provide valid ratings JSON'}))
-    return
-  }
+    try {
+        ratings = JSON.parse(req.body)
+    } catch (error) {
+        res.writeHead(400, {'Content-type': 'application/json'})
+        res.end(JSON.stringify({error: 'please provide valid ratings JSON'}))
+        return
+    }
 
-  if (process.env.SERVICE_VERSION === 'v2') { // the version that is backed by a database
-    res.writeHead(501, {'Content-type': 'application/json'})
-    res.end(JSON.stringify({error: 'Post not implemented for database backed ratings'}))
-  } else { // the version that holds ratings in-memory
-    res.writeHead(200, {'Content-type': 'application/json'})
-    res.end(JSON.stringify(putLocalReviews(productId, ratings)))
-  }
+    if (process.env.SERVICE_VERSION === 'v2') { // the version that is backed by a database
+        res.writeHead(501, {'Content-type': 'application/json'})
+        res.end(JSON.stringify({error: 'Post not implemented for database backed ratings'}))
+    } else { // the version that holds ratings in-memory
+        res.writeHead(200, {'Content-type': 'application/json'})
+        res.end(JSON.stringify(putLocalReviews(productId, ratings)))
+    }
 })
 
 dispatcher.onGet(/^\/ratings\/[0-9]*/, function (req, res) {
-  var productIdStr = req.url.split('/').pop()
-  var productId = parseInt(productIdStr)
+    var productIdStr = req.url.split('/').pop()
+    var productId = parseInt(productIdStr)
 
-  if (Number.isNaN(productId)) {
-    res.writeHead(400, {'Content-type': 'application/json'})
-    res.end(JSON.stringify({error: 'please provide numeric product ID'}))
-  } else if (process.env.SERVICE_VERSION === 'v2') {
-    var firstRating = 0
-    var secondRating = 0
+    if (Number.isNaN(productId)) {
+        res.writeHead(400, {'Content-type': 'application/json'})
+        res.end(JSON.stringify({error: 'please provide numeric product ID'}))
+    } else if (process.env.SERVICE_VERSION === 'v2') {
+        var firstRating = 0
+        var secondRating = 0
 
-    if (process.env.DB_TYPE === 'mysql') {
-      var connection = mysql.createConnection({
-        host: hostName,
-        port: portNumber,
-        user: username,
-        password: password,
-        database: 'test'
-      })
+        if (process.env.DB_TYPE === 'mysql') {
+            var connection = mysql.createConnection({
+                host: hostName,
+                port: portNumber,
+                user: username,
+                password: password,
+                database: 'test'
+            })
 
-      connection.connect(function(err) {
-          if (err) {
-              res.end(JSON.stringify({error: 'could not connect to ratings database'}))
-              console.log(err)
-              return
-          }
-          connection.query('SELECT Rating FROM ratings', function (err, results, fields) {
-              if (err) {
-                  res.writeHead(500, {'Content-type': 'application/json'})
-                  res.end(JSON.stringify({error: 'could not perform select'}))
-                  console.log(err)
-              } else {
-                  if (results[0]) {
-                      firstRating = results[0].Rating
-                  }
-                  if (results[1]) {
-                      secondRating = results[1].Rating
-                  }
-                  var result = {
-                      id: productId,
-                      ratings: {
-                          Reviewer1: firstRating,
-                          Reviewer2: secondRating
-                      }
-                  }
-                  res.writeHead(200, {'Content-type': 'application/json'})
-                  res.end(JSON.stringify(result))
-              }
-          })
-          // close the connection
-          connection.end()
-      })
-    } else {
-      MongoClient.connect(url, function (err, client) {
-        if (err) {
-          res.writeHead(500, {'Content-type': 'application/json'})
-          res.end(JSON.stringify({error: 'could not connect to ratings database'}))
-          console.log(err)
-        } else {
-          const db = client.db("test")
-          db.collection('ratings').find({}).toArray(function (err, data) {
-            if (err) {
-              res.writeHead(500, {'Content-type': 'application/json'})
-              res.end(JSON.stringify({error: 'could not load ratings from database'}))
-              console.log(err)
-            } else {
-              if (data[0]) {
-                firstRating = data[0].rating
-              }
-              if (data[1]) {
-                secondRating = data[1].rating
-              }
-              var result = {
-                id: productId,
-                ratings: {
-                  Reviewer1: firstRating,
-                  Reviewer2: secondRating
+            connection.connect(function (err) {
+                if (err) {
+                    res.end(JSON.stringify({error: 'could not connect to ratings database'}))
+                    console.log(err)
+                    return
                 }
-              }
-              res.writeHead(200, {'Content-type': 'application/json'})
-              res.end(JSON.stringify(result))
+                connection.query('SELECT Rating FROM ratings', function (err, results, fields) {
+                    if (err) {
+                        res.writeHead(500, {'Content-type': 'application/json'})
+                        res.end(JSON.stringify({error: 'could not perform select'}))
+                        console.log(err)
+                    } else {
+                        if (results[0]) {
+                            firstRating = results[0].Rating
+                        }
+                        if (results[1]) {
+                            secondRating = results[1].Rating
+                        }
+                        var result = {
+                            id: productId,
+                            ratings: {
+                                Reviewer1: firstRating,
+                                Reviewer2: secondRating
+                            }
+                        }
+                        res.writeHead(200, {'Content-type': 'application/json'})
+                        res.end(JSON.stringify(result))
+                    }
+                })
+                // close the connection
+                connection.end()
+            })
+        } else if (process.env.DB_TYPE === 'oracle') {
+            oracledb.getConnection(dbConfig, function (err, conn) {
+                if (err) {
+                    res.end(JSON.stringify({error: 'could not connect to ratings database'}));
+                    console.log('Error getting connection', err);
+                    return;
+                }
+
+                conn.execute('SELECT Rating FROM ratings', [], {outFormat: oracledb.OBJECT},
+                    function (err, results) {
+                        if (err) {
+                            res.writeHead(500, {'Content-type': 'application/json'});
+                            res.end(JSON.stringify({error: 'could not perform select'}));
+
+                            conn.close(function (err) {
+                                if (err) {
+                                    console.log('Error closing connection', err);
+                                } else {
+                                    console.log('Connection closed');
+                                }
+                            });
+                            return;
+                        }
+
+                        console.log('Query executed', results);
+                        if (results.rows[0]) {
+                            firstRating = results.rows[0].RATING;
+                        }
+                        if (results.rows[1]) {
+                            secondRating = results.rows[1].RATING;
+                        }
+
+                        const result = {
+                            id: productId,
+                            ratings: {
+                                Reviewer1: firstRating,
+                                Reviewer2: secondRating
+                            }
+                        };
+                        res.writeHead(200, {'Content-type': 'application/json'});
+                        res.end(JSON.stringify(result));
+                    }
+                );
+
+                // close the connection
+                conn.close(function (err) {
+                    if (err) {
+                        console.log('Error closing connection', err);
+                    } else {
+                        console.log('Connection closed');
+                    }
+                });
+            });
+        } else {
+            MongoClient.connect(url, function (err, client) {
+                if (err) {
+                    res.writeHead(500, {'Content-type': 'application/json'})
+                    res.end(JSON.stringify({error: 'could not connect to ratings database'}))
+                    console.log(err)
+                } else {
+                    const db = client.db("test")
+                    db.collection('ratings').find({}).toArray(function (err, data) {
+                        if (err) {
+                            res.writeHead(500, {'Content-type': 'application/json'})
+                            res.end(JSON.stringify({error: 'could not load ratings from database'}))
+                            console.log(err)
+                        } else {
+                            if (data[0]) {
+                                firstRating = data[0].rating
+                            }
+                            if (data[1]) {
+                                secondRating = data[1].rating
+                            }
+                            var result = {
+                                id: productId,
+                                ratings: {
+                                    Reviewer1: firstRating,
+                                    Reviewer2: secondRating
+                                }
+                            }
+                            res.writeHead(200, {'Content-type': 'application/json'})
+                            res.end(JSON.stringify(result))
+                        }
+                        // close client once done:
+                        client.close()
+                    })
+                }
+            })
+        }
+    } else {
+        if (process.env.SERVICE_VERSION === 'v-faulty') {
+            // in half of the cases return error,
+            // in another half proceed as usual
+            var random = Math.random(); // returns [0,1]
+            if (random <= 0.5) {
+                getLocalReviewsServiceUnavailable(res)
+            } else {
+                getLocalReviewsSuccessful(res, productId)
             }
-            // close client once done:
-            client.close()
-          })
+        } else if (process.env.SERVICE_VERSION === 'v-delayed') {
+            // in half of the cases delay for 7 seconds,
+            // in another half proceed as usual
+            var random = Math.random(); // returns [0,1]
+            if (random <= 0.5) {
+                setTimeout(getLocalReviewsSuccessful, 7000, res, productId)
+            } else {
+                getLocalReviewsSuccessful(res, productId)
+            }
+        } else if (process.env.SERVICE_VERSION === 'v-unavailable' || process.env.SERVICE_VERSION === 'v-unhealthy') {
+            if (unavailable) {
+                getLocalReviewsServiceUnavailable(res)
+            } else {
+                getLocalReviewsSuccessful(res, productId)
+            }
+        } else {
+            getLocalReviewsSuccessful(res, productId)
         }
-      })
     }
-  } else {
-      if (process.env.SERVICE_VERSION === 'v-faulty') {
-        // in half of the cases return error,
-        // in another half proceed as usual
-        var random = Math.random(); // returns [0,1]
-        if (random <= 0.5) {
-          getLocalReviewsServiceUnavailable(res)
-        } else {
-          getLocalReviewsSuccessful(res, productId)
-        }
-      }
-      else if (process.env.SERVICE_VERSION === 'v-delayed') {
-        // in half of the cases delay for 7 seconds,
-        // in another half proceed as usual
-        var random = Math.random(); // returns [0,1]
-        if (random <= 0.5) {
-          setTimeout(getLocalReviewsSuccessful, 7000, res, productId)
-        } else {
-          getLocalReviewsSuccessful(res, productId)
-        }
-      }
-      else if (process.env.SERVICE_VERSION === 'v-unavailable' || process.env.SERVICE_VERSION === 'v-unhealthy') {
-          if (unavailable) {
-              getLocalReviewsServiceUnavailable(res)
-          } else {
-              getLocalReviewsSuccessful(res, productId)
-          }
-      }
-      else {
-        getLocalReviewsSuccessful(res, productId)
-      }
-  }
 })
 
 dispatcher.onGet('/health', function (req, res) {
@@ -218,48 +275,48 @@ dispatcher.onGet('/health', function (req, res) {
     }
 })
 
-function putLocalReviews (productId, ratings) {
-  userAddedRatings[productId] = {
-    id: productId,
-    ratings: ratings
-  }
-  return getLocalReviews(productId)
+function putLocalReviews(productId, ratings) {
+    userAddedRatings[productId] = {
+        id: productId,
+        ratings: ratings
+    }
+    return getLocalReviews(productId)
 }
 
 function getLocalReviewsSuccessful(res, productId) {
-  res.writeHead(200, {'Content-type': 'application/json'})
-  res.end(JSON.stringify(getLocalReviews(productId)))
+    res.writeHead(200, {'Content-type': 'application/json'})
+    res.end(JSON.stringify(getLocalReviews(productId)))
 }
 
 function getLocalReviewsServiceUnavailable(res) {
-  res.writeHead(503, {'Content-type': 'application/json'})
-  res.end(JSON.stringify({error: 'Service unavailable'}))
+    res.writeHead(503, {'Content-type': 'application/json'})
+    res.end(JSON.stringify({error: 'Service unavailable'}))
 }
 
-function getLocalReviews (productId) {
-  if (typeof userAddedRatings[productId] !== 'undefined') {
-      return userAddedRatings[productId]
-  }
-  return {
-    id: productId,
-    ratings: {
-      'Reviewer1': 5,
-      'Reviewer2': 4
+function getLocalReviews(productId) {
+    if (typeof userAddedRatings[productId] !== 'undefined') {
+        return userAddedRatings[productId]
     }
-  }
+    return {
+        id: productId,
+        ratings: {
+            'Reviewer1': 5,
+            'Reviewer2': 4
+        }
+    }
 }
 
-function handleRequest (request, response) {
-  try {
-    console.log(request.method + ' ' + request.url)
-    dispatcher.dispatch(request, response)
-  } catch (err) {
-    console.log(err)
-  }
+function handleRequest(request, response) {
+    try {
+        console.log(request.method + ' ' + request.url)
+        dispatcher.dispatch(request, response)
+    } catch (err) {
+        console.log(err)
+    }
 }
 
 var server = http.createServer(handleRequest)
 
 server.listen(port, function () {
-  console.log('Server listening on: http://0.0.0.0:%s', port)
+    console.log('Server listening on: http://0.0.0.0:%s', port)
 })
